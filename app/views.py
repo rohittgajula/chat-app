2a4ea48ef607e937ce5f3313b0836aa0afdd5870
Rohit Gajula <rohitgajula27@gmail.com>
2024-09-14 00:20:45 +0530

    indexpage

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from .models import ChatRoom
 
 
 def index(request):
-  return render(request, 'app/index.html')
+  chatrooms = ChatRoom.objects.all()
+  return render(request, 'app/index.html', {'chatrooms':chatrooms})
+
+def chatroom(request, slug):
+  chatroom = ChatRoom.objects.get(slug=slug)
+
+  return render(request, 'app/room.html', {'chatroom':chatroom})
